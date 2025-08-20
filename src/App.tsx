e3e4bf833987ece8a82ce4121d913658b937dddd
kimywann <kimywan10@gmail.com>
2025-08-20 21:34:29 +0900

    chore: @vercel/speed-insights 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "./common/lib/supabaseClient";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { Toaster } from "sonner";
 
 function App() {
   return (
     <SessionContextProvider supabaseClient={supabase}>
+      <SpeedInsights />
       <Analytics />
       <Toaster richColors position="top-right" />
       <RouterProvider router={router} />
