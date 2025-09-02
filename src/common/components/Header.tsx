7efd68e0a152d0852e9fa86a206f6bcf5cd558d6
kimywann <kimywan10@gmail.com>
2025-09-02 11:20:30 +0900

    feat: 헤더 로그인 시 nav 바 구현

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -37,11 +37,33 @@ function Header() {
   };
 
   return (
-    <div className="mt-4 flex justify-between">
-      <Link to="/">
-        <div className="font-bold text-indigo-600">WiseSub</div>
-      </Link>
-      <div className="flex gap-1.5">
+    <header className="mt-4 flex justify-between">
+      <div className="flex items-center gap-6">
+        <h1 className="font-bold text-indigo-600">
+          <Link to="/">WiseSub</Link>
+        </h1>
+
+        <nav className="flex gap-2">
+          {user && (
+            <>
+              <Link
+                to="/subscription"
+                className="cursor-pointer rounded-md px-4 py-1 text-slate-600 hover:bg-slate-100"
+              >
+                대시보드
+              </Link>
+              <Link
+                to="/chart"
+                className="cursor-pointer rounded-md px-4 py-1 text-slate-600 hover:bg-slate-100"
+              >
+                차트
+              </Link>
+            </>
+          )}
+        </nav>
+      </div>
+
+      <section className="flex gap-1.5">
         {user ? (
           <Button size="sm" variant="primary" onClick={handleLogout}>
             로그아웃
@@ -60,8 +82,8 @@ function Header() {
             </Link>
           </>
         )}
-      </div>
-    </div>
+      </section>
+    </header>
   );
 }
 
