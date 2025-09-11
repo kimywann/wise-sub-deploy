c07b2434189aa3669564bd8882ece631d37e7d9f
kimywann <kimywan10@gmail.com>
2025-09-11 10:26:07 +0900

    refactor: 사용하지 않는 nickname, birthYear 필드 제거

--- src/features/auth/api/sign-up.ts
+++ src/features/auth/api/sign-up.ts
@@ -3,13 +3,9 @@ import { supabase } from "@/common/lib/supabaseClient";
 export async function signup({
   email,
   password,
-  nickname,
-  birthYear,
 }: {
   email: string;
   password: string;
-  nickname: string;
-  birthYear: number;
 }) {
   const { data, error } = await supabase.auth.signUp({
     email,
@@ -18,19 +14,6 @@ export async function signup({
 
   if (error || !data.user) {
     throw new Error(error?.message || "회원가입 실패");
-  }
-
-  // 프로필 테이블에 nickname, birthYear 저장
-  const { error: insertError } = await supabase.from("user_detail").insert([
-    {
-      id: data.user.id,
-      nickname,
-      birth_year: birthYear,
-    },
-  ]);
-
-  if (insertError) {
-    throw new Error(insertError.message || "프로필 생성 실패");
   }
 
   return data.user;
