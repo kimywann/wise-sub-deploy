ba2c121abb76df498d9ba9b89b868b8ebe5deb5e
kimywann <kimywan10@gmail.com>
2025-09-16 11:52:15 +0900

    chore: 폴더 경로 개선

--- src/pages/AddSubscriptionPage.tsx
+++ src/pages/AddSubscriptionPage.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 import leftIcon from "@/assets/icon/left.svg";
-import ServiceBox from "@/components/service/ServiceBox";
+import ServiceBox from "@/components/dashboard/ServiceBox";
 
 function AddSubscriptionPage() {
   const navigate = useNavigate();
