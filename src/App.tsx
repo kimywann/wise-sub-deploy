14a841eabe17fc19893d5876671c4340792b3685
kimywann <kimywan10@gmail.com>
2025-09-16 11:35:11 +0900

    feat: Analytics 컴포넌트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,11 @@ import router from "./router";
 
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "./supabaseClient";
-
-import { Analytics } from "@vercel/analytics/react";
-
 import { Toaster } from "sonner";
 
 function App() {
   return (
     <SessionContextProvider supabaseClient={supabase}>
-      <Analytics />
       <Toaster richColors position="top-right" />
       <RouterProvider router={router} />
     </SessionContextProvider>
